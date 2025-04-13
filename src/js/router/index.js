ce5ff0a507816b5f04b1a879d5887e947a73f033
Maya Thompson <majsant96@hotmail.com>
2025-04-13 14:54:54 +0900

    add about modal file and content with basic bootstrap styling and update all navbars to include modal id

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -18,9 +18,6 @@ export default async function router(pathname = window.location.pathname) {
     case "/post/edit/":
       await import("./views/postEdit.js");
       break;
-    case "/post/create/":
-      await import("./views/postCreate.js");
-      break;
     case "/profile/":
       await import("./views/profile.js");
       break;
